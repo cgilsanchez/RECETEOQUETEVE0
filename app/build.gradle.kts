e023d10c0733354d477a63ba8f17691f5532b0ee
cgilsanchez <cgilsanchez.02@gmail.com>
2024-11-25 08:37:52 +0100

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,47 +29,65 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 }
 
 dependencies {
+    // Room
     implementation(libs.androidx.room.runtime)
-    annotationProcessor(libs.androidx.room.room.compiler)
-
-    // To use Kotlin annotation processing tool (kapt)
     kapt(libs.androidx.room.room.compiler)
-    kapt(libs.hilt.android.compiler)
-
-    // optional - Kotlin Extensions and Coroutines support for Room
     implementation(libs.androidx.room.ktx)
 
-    implementation (libs.cardview.v7)
-    implementation (libs.design)
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // Retrofit y Gson
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
-    implementation(libs.androidx.fragment.ktx)
-    implementation(libs.hilt.android)
-    implementation(libs.androidx.navigation.fragment)
-    implementation(libs.androidx.navigation.ui)
+
+    // AndroidX
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Material Design
+    implementation(libs.material)
+
+    // Navegación
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    // Fragment y Activity
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.activity)
+
+    // CardView y diseño de interfaz
+    implementation(libs.cardview.v7)
+    implementation(libs.design)
+
+    // Carga de imágenes
+    implementation(libs.coil)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.coil)
 }
+
 kapt {
     correctErrorTypes = true
 }
---
+++ app/src/main/java/com/example/receteo/ui/auth/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.receteo.ui.auth;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
