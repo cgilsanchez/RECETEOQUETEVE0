e288978f35fbd8b4b16ba9c4a3db7aaf7f45a4ce
cgilsanchez <cgilsanchez.02@gmail.com>
2024-11-26 10:58:34 +0100

    first commit

--- app/src/main/java/com/example/receteo/MainActivity.kt
+++ app/src/main/java/com/example/receteo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.receteo
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -10,26 +11,39 @@ import com.example.receteo.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Crear un Bundle vacío si savedInstanceState es null
-        val nonNullSavedInstanceState = savedInstanceState ?: Bundle()
-        super.onCreate(nonNullSavedInstanceState)
+        super.onCreate(savedInstanceState)
 
+        // Establece el layout principal
         setContentView(R.layout.activity_main)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setupWithNavController(navController)
+        // Manejo robusto para evitar errores
+        initializeNavigation()
     }
 
+    private fun initializeNavigation() {
+        try {
+            // Inicializa el NavHostFragment
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                    as? NavHostFragment ?: throw IllegalStateException("NavHostFragment no encontrado")
+            navController = navHostFragment.navController
 
+            // Configura el BottomNavigationView
+            val bottomNavigationView: BottomNavigationView =
+                findViewById(R.id.bottom_navigation)
+            bottomNavigationView.setupWithNavController(navController)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("MainActivity", "Error durante la inicialización: ${e.message}")
+        }
+    }
 }
+
+
+
--- app/src/main/java/com/example/receteo/ui/recipes/AddRecipeFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.receteo.ui.recipes;
-
-import androidx.fragment.app.Fragment;
-
-public class AddRecipeFragment extends Fragment {
-}
