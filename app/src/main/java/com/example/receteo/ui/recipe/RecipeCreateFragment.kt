b57fb239082de294dc756a4ab33836baeca23494
cgilsanchez <cgilsanchez.02@gmail.com>
2025-02-23 13:47:40 +0100

    aparece la misma foto si actualizo y no toco la foto solo el nombre o lo que sea

--- app/src/main/java/com/example/receteo/ui/recipe/RecipeCreateFragment.kt
+++ app/src/main/java/com/example/receteo/ui/recipe/RecipeCreateFragment.kt
@@ -105,6 +105,7 @@ class RecipeCreateFragment : Fragment() {
     }
 
 
+
     private fun openGallery() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         imagePickerLauncher.launch(intent)
