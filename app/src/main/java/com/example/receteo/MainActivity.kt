7a024ebc680a1a3b27eb30acc207a7640c5f9a5b
cgilsanchez <cgilsanchez.02@gmail.com>
2025-02-24 13:03:36 +0100

    aparece la misma foto si actualizo y no toco la foto solo el nombre o lo que sea

--- app/src/main/java/com/example/receteo/MainActivity.kt
+++ app/src/main/java/com/example/receteo/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.receteo
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -17,6 +22,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ✅ SOLICITAR PERMISOS DE NOTIFICACIÓN EN ANDROID 13+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+        }
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
@@ -44,4 +57,6 @@ class MainActivity : AppCompatActivity() {
         val sharedPreferences = getSharedPreferences("auth_prefs", MODE_PRIVATE)
         return sharedPreferences.getString("jwt", null) != null
     }
+
+
 }
--- app/src/main/java/com/example/receteo/RecipeApplication.kt
+++ app/src/main/java/com/example/receteo/RecipeApplication.kt
@@ -1,8 +1,10 @@
 package com.example.receteo
 
 import android.app.Application
+import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -11,6 +13,13 @@ class RecipeApplication : Application(), Configuration.Provider {
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
+
+    override fun onCreate() {
+        super.onCreate()
+        WorkManager.getInstance(this).pruneWork() // Elimina trabajos previos para evitar conflictos
+        Log.d("RecipeWorker", "⚙️ WorkManager inicializado correctamente")
+    }
+
 
     override fun getWorkManagerConfiguration() =
         Configuration.Builder()
